e8bdd7d8c665c2e5730fb1a228317110d22f6215
gamifica-CED14 <gamificaced14@gmail.com>
2026-06-30 10:29:11 -0300

    adicionando projetos do computador 09

---
+++ 3E/Lorrany e Maria Clara  3 E/Assets/script/Cereja.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Cereja : MonoBehaviour
+{
+   private SpriteRenderer sr;
+    private CircleCollider2D circle;
+    public GameObject collected;
+    // Start is called before the first frame update
+    void Start()
+    {
+    sr = GetComponent<SpriteRenderer>();
+    circle = GetComponent<CircleCollider2D>();
+    }
+    // Quando algo encostar na maçã
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+    // Verifica se foi o Player
+    if (collider.gameObject.tag == "Player")
+    {
+    sr.enabled = false;
+    circle.enabled = false;
+    collected.SetActive(true);
+    // Coleta a maçã (destrói após 1 segundo)
+    Destroy(gameObject, 0.3f);
+    }
+    }
+}
+
