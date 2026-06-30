840dab8188aa9e70d6ac5679406e06c6edd9a922
gamifica-CED14 <gamificaced14@gmail.com>
2026-06-30 10:13:55 -0300

    adicionando projetos do computador 11

---
+++ 3E/Danielly e Kátia 3E/Assets/Script/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target; 
+    private Vector3 velocity = Vector3.zero; 
+ 
+    [SerializeField] private float smoothTime = 0.1f; 
+ 
+    void Start() 
+    { 
+        target = GameObject.FindWithTag("Player").transform; 
+    } 
+ 
+    void Update() 
+    { 
+        Vector3 cameraPosition = target.position + new Vector3(0, 0, -10f); 
+        transform.position = Vector3.SmoothDamp(transform.position, cameraPosition, ref 
+velocity, smoothTime); 
+    } 
+} 
