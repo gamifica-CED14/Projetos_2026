6aecfcb2fa717701c1c15a345a2d3b377768c6de
gamifica-CED14 <gamificaced14@gmail.com>
2026-07-01 07:34:42 -0300

    adicionando projetos do computador 21

---
+++ 3B/Isabelly 3B/Assets/Script/ControlePlayer.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class ControlePlayer : MonoBehaviour
+{  
+      [Header("Movimento")]
+    public float velocidade = 5f;
+    public float forcaPulo = 10f;
+    [Header("Detecção de Chão")]
+    public Transform checadorChao;
+    public float raioChao = 0.2f;
+    public LayerMask layerChao;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private float movimentoX;
+    private bool noChao;
+    private int quantidadePulos = 0;
+    public int maxPulos = 2;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movimentoX = Input.GetAxisRaw("Horizontal");
+        // Detecta chão
+        noChao = Physics2D.OverlapCircle(
+            checadorChao.position,
+            raioChao,
+            layerChao
+        );
+        if (noChao)
+        {
+            quantidadePulos = 0;
+        }
+        if (movimentoX > 0)
+            sprite.flipX = false;
+        else if (movimentoX < 0)
+            sprite.flipX = true;
+        if (movimentoX != 0)
+            anim.SetBool("walk", true);
+        else
+            anim.SetBool("walk", false);
+        if (!noChao)
+            anim.SetBool("jump", true);
+        else
+            anim.SetBool("jump", false);
+        if (Input.GetKeyDown(KeyCode.Space)
+            && quantidadePulos < maxPulos)
+        {
+            rb.linearVelocity = new Vector2(
+                rb.linearVelocity.x,
+                forcaPulo
+            );
+            quantidadePulos++;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(
+            movimentoX * velocidade,
+            rb.linearVelocity.y
+        );
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (checadorChao != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(
+                checadorChao.position,
+                raioChao
+            );
+        }
+    }
+}
---
+++ 3C/Nicolle Cristina 3C/Assets/script/HeartSystem.cs
@@ -0,0 +1,50 @@
+using UnityEngine; 
+using UnityEngine.UI; 
+ 
+public class HeartSystem : MonoBehaviour 
+{ 
+    public int vida = 5; 
+    public int vidaMaxima = 5; 
+ 
+    public Image[] coracoes; 
+    public Sprite cheio; 
+    public Sprite vazio; 
+ 
+    void Update() 
+    { 
+        if (vida > vidaMaxima) 
+            vida = vidaMaxima; 
+ 
+        AtualizarCoracoes(); 
+    } 
+ 
+    void AtualizarCoracoes() 
+    { 
+        for (int i = 0; i < coracoes.Length; i++) 
+        { 
+            coracoes[i].enabled = (i < vidaMaxima); 
+ 
+            if (i < vida) 
+                coracoes[i].sprite = cheio; 
+            else 
+                coracoes[i].sprite = vazio; 
+        } 
+    } 
+ 
+    public void ReceberDano(int dano) 
+    { 
+        vida -= dano; 
+ 
+        if (vida <= 0) 
+        { 
+            vida = 0; 
+            Morrer(); 
+        } 
+    } 
+ 
+    void Morrer() 
+    { 
+        Debug.Log("Personagem morreu!"); 
+        gameObject.SetActive(false); 
+    } 
+}
